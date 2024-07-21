dbc85632120a30d028ab8d0013f3c79f2bf3d81a
vw-shane <wagen00@gmail.com>
2024-07-21 16:24:56 -0400

    seems to working now, issue is the next previous buttons only seem to work localy

--- app.js
+++ app.js
@@ -588,7 +588,7 @@ app.post('/admin', upload.array('images', 10), async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
